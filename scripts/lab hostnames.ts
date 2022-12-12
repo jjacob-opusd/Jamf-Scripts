eed2423dabed5d9cd161998fc5a0acd2a91eaf76
jjacob-opusd <120224789+jjacob-opusd@users.noreply.github.com>
2022-12-12 13:21:47 -0800

    change to single quotes

--- scripts/lab hostnames.ts
+++ scripts/lab hostnames.ts
@@ -1,21 +1,21 @@
-import { cmdOutput } from "./common/cmd.ts";
-import { parsePlist } from "./common/plist.ts";
-import { ComputersService } from "./openapi/jamf/classic/index.ts";
+import { cmdOutput } from './common/cmd.ts';
+import { parsePlist } from './common/plist.ts';
+import { ComputersService } from './openapi/jamf/classic/index.ts';
 
 const JAMFConnection = {
-  server: "https://oakpark.jamfcloud.com/JSSResource",
-  username: "api",
-  password: "f9Akf*pk%H&ZZ;uXJg",
+	server: 'https://oakpark.jamfcloud.com/JSSResource',
+	username: 'api',
+	password: 'f9Akf*pk%H&ZZ;uXJg',
 };
 
 async function SPHardwareDataType() {
-  const output = await cmdOutput([
-    "system_profiler",
-    "-xml",
-    "SPHardwareDataType",
-  ]);
-  const plist = await parsePlist(output);
-  return plist[0]._items[0];
+	const output = await cmdOutput([
+		'system_profiler',
+		'-xml',
+		'SPHardwareDataType',
+	]);
+	const plist = await parsePlist(output);
+	return plist[0]._items[0];
 }
 const hardware = await SPHardwareDataType();
 
@@ -33,41 +33,41 @@ const hardware = await SPHardwareDataType();
 //   },
 // ).then((res) => res.json());
 const computerResponse = await ComputersService.findComputersBySerialNumber({
-  serialnumber: hardware.serial_number,
+	serialnumber: hardware.serial_number,
 });
 
 const buildingShortcodes = {
-  BES: "B",
-  DO: "DO",
-  EC: "EC",
-  MCMS: "MC",
-  OHES: "OH",
-  OPHS: "HS",
-  OPIS: "IS",
-  OPNS: "NS",
-  OVHS: "OV",
-  ROES: "R",
-  Technology: "IT",
+	BES: 'B',
+	DO: 'DO',
+	EC: 'EC',
+	MCMS: 'MC',
+	OHES: 'OH',
+	OPHS: 'HS',
+	OPIS: 'IS',
+	OPNS: 'NS',
+	OVHS: 'OV',
+	ROES: 'R',
+	Technology: 'IT',
 };
 const building: keyof typeof buildingShortcodes =
-  computerResponse.computer.location.building;
-const buildingShortcode = buildingShortcodes[building] || "NONE";
+	computerResponse.computer.location.building;
+const buildingShortcode = buildingShortcodes[building] || 'NONE';
 
-const assetTag = computerResponse.computer.general.asset_tag.replace("-", "");
+const assetTag = computerResponse.computer.general.asset_tag.replace('-', '');
 
 const modelShortcodes = {
-  macbookpro: "MBP",
-  macbookair: "MBA",
-  macbook: "MB",
-  macmini: "MM",
-  macstudio: "MS",
-  macpro: "MP",
-  powermac: "PM",
+	macbookpro: 'MBP',
+	macbookair: 'MBA',
+	macbook: 'MB',
+	macmini: 'MM',
+	macstudio: 'MS',
+	macpro: 'MP',
+	powermac: 'PM',
 };
 
 const modelIdentifier: keyof typeof modelShortcodes = hardware.machine_model
-  .replace(/[0-9,]/g, "")
-  .toLowerCase();
+	.replace(/[0-9,]/g, '')
+	.toLowerCase();
 const modelShortcode = modelShortcodes[modelIdentifier];
 
 const hostname = `${buildingShortcode}-${assetTag}-${modelShortcode}`;
@@ -80,4 +80,4 @@ console.log({ hostname });
 // await cmdOutput(['scutil', '--set', 'LocalHostName', hostname]);
 // await cmdOutput(['jamf', 'recon']);
 // await cmdOutput(['jamf', 'policy']);
-console.log("Done");
+console.log('Done');
