099b24126a1b71737fab29d234a76465d9b5ba1c
jjacob-opusd <120224789+jjacob-opusd@users.noreply.github.com>
2022-12-15 14:59:30 -0800

    cache-served function

---
+++ scripts/cache-served.ts
@@ -0,0 +1,28 @@
+import { format } from 'https://deno.land/std@0.167.0/fmt/bytes.ts';
+import { cmdOutput } from './common/cmd.ts';
+
+const columns = [
+	'ZBYTESFROMCACHETOCHILD',
+	'ZBYTESFROMCACHETOCLIENT',
+	'ZBYTESFROMCACHETOPEER',
+	'ZBYTESFROMORIGINTOCHILD',
+	'ZBYTESFROMORIGINTOCLIENT',
+	'ZBYTESFROMORIGINTOPEER',
+	'ZBYTESFROMPARENTTOCHILD',
+	'ZBYTESFROMPARENTTOCLIENT',
+	'ZBYTESFROMPARENTTOPEER',
+	'ZBYTESFROMPEERTOCHILD',
+	'ZBYTESFROMPEERTOCLIENT',
+];
+
+const joinedColumns = columns.map((c) => `SUM(${c})`).join(', ');
+const commands = `.open /Library/Application\ Support/Apple/AssetCache/Metrics/Metrics.db
+SELECT ${joinedColumns} FROM ZMETRIC;`;
+
+const output = await cmdOutput(['zsh', '-c', `echo "${commands}" | sqlite3`]);
+const total = output
+	.split('|')
+	.map((c) => parseInt(c))
+	.reduce((a, b) => a + b);
+
+console.log(`<result>${format(total)}</result>`);
